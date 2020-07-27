7716e64bc2a8160677fb8003c77a9773d1acf38b
Nicole Gile <nicolegile94@gmail.com>
2020-07-26 20:15:26 -0500

    added questions

--- index.js
+++ index.js
@@ -1,44 +1,91 @@
 //const fs = require('fs');
 //const generateMarkdown = require('./Develop/utils/generateMarkdown.js');
 const inquirer = require('inquirer');
-console.log(inquirer);
+const { flatMap } = require('lodash');
+
+
 // array of questions for user
-inquirer
-    .prompt([
+const promptUser = () => {
+    console.log(`
+    =================
+    Add a New Read Me
+    =================
+    `);
+    return inquirer.prompt([
         {
             type: 'input',
-            name: 'File Name',
-            message: "What is the name of your file?"
+            name: 'file',
+            message: 'What is the name of your file? (Required)',
+            validate: fileInput => {
+                if (fileInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a file name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? (Required)',
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter installation steps!');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions and examples for use. (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter instructions');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'List your collaborators, if any, with links to their GitHub profiles. (Required)',
+            validate: creditsInput => {
+                if (creditsInput) {
+                    return true;
+                } else {
+                    console.log('Please list your collaborators!');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/). (Required)',
+            validate: licenseInput => {
+                if (licenseInput) {
+                    return true;
+                } else {
+                    console.log('Please enter license information!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Add guidelines for how you would like other developers to contribute to this project'
         }
-    ])
-    .then(answers => console.log(answers));
+    ]); 
+};
+promptUser().then(answers => console.log(answers));
 const questions = [
-
-    //installation (Required)
-    //What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-
-    //Usage (Required)
-    //Provide instructions and examples for use. Include screenshots as needed. 
-
-    //Credits (Required)
-    //List your collaborators, if any, with links to their GitHub profiles.
-    //If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    //If you followed tutorials, include links to those here as well.
-
-
-    //License (Required)
-    //The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
-
-    //Badges
-    //Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-    //Contributing
-    //If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
-
-    //Tests
-    //Go the extra mile and write tests for your application. Then provide examples on how to run them.
-
 
 ];
 
